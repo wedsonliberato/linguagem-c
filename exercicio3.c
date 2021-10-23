f34c84c537fa44311ff88a27acc5e49b759f9b3d
wedsonliberato <93042703+wedsonliberato@users.noreply.github.com>
2021-10-23 15:32:06 -0300

    Create exercicio3.c

---
+++ exercicio3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1 = 5;
+    
+    int num2 = 15;
+    
+    
+    if (num1 > num2){
+        
+     printf("num1 é maior que num2");   
+        
+    } else {
+        
+     printf("num1 não é maior que num2");   
+        
+    }
+
+    return 0;
+}
